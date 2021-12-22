034e44564f68c959ea530922f2fe9362f850e575
Sebastian Stuckenbrock <sstuckenbrock@efhm.de>
2021-12-22 20:18:50 +0100

    Refactor Update-Script. Docker-Build now included.

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,14 @@
-ARG BASE_TAG=bionic-20210930
+ARG BASE_IMAGE_TAG=bionic-20210930
 
-FROM ubuntu:${BASE_TAG}
+FROM ubuntu:${BASE_IMAGE_TAG}
 
 MAINTAINER Sebastian Stuckenbrock sstuckenbrock@efhm.de
 
-ARG MEDIAWIKI_VERSION=1.37
-ARG MEDIAWIKI_PATCH=1
-ARG MEDIAWIKI_TARBALL=https://releases.wikimedia.org/mediawiki/${MEDIAWIKI_VERSION}/mediawiki-${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}.tar.gz
-
-# Build-time metadata as defined at http://label-schema.org
-ARG BUILD_DATE
-ARG VCS_REF
-ARG VERSION
-LABEL org.label-schema.build-date=${BUILD_DATE} \
-      org.label-schema.name="Mediawiki" \
-      org.label-schema.description="Simple Mediawiki Container with mysql-server included." \
-      org.label-schema.url="https://hub.docker.com/r/stucky/mediawiki" \
-      org.label-schema.vcs-ref=${VCS_REF} \
-      org.label-schema.vcs-url="https://github.com/stuckyhm/docker-mediawiki" \
-      org.label-schema.version=${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}-${BASE_TAG} \
-      org.label-schema.schema-version="1.0"
+ARG MEDIAWIKI_VERSION=1.37.1
+ARG MEDIAWIKI_VERSION_MAJOR=1
+ARG MEDIAWIKI_VERSION_MINOR=37
+ARG MEDIAWIKI_VERSION_PATCH=1 
+ARG MEDIAWIKI_TARBALL=https://releases.wikimedia.org/mediawiki/${MEDIAWIKI_VERSION_MAJOR}.${MEDIAWIKI_VERSION_MINOR}/mediawiki-${MEDIAWIKI_VERSION}.tar.gz
 
 RUN apt-get update \
 	&& DEBIAN_FRONTEND=noninteractive apt-get -y install apt-transport-https apt-utils curl gpg
@@ -55,8 +44,8 @@ RUN mkdir /var/run/mysqld \
 	&& chmod 777 /var/run/mysqld
 
 RUN cd /var/www \
-	&& wget -O mediawiki-${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}.tar.gz ${MEDIAWIKI_TARBALL} \
-	&& tar -xf mediawiki-${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}.tar.gz && rm *.tar.gz \
+	&& wget -O mediawiki-${MEDIAWIKI_VERSION}.tar.gz ${MEDIAWIKI_TARBALL} \
+	&& tar -xf mediawiki-${MEDIAWIKI_VERSION}.tar.gz && rm *.tar.gz \
 	&& mv mediawiki-* mediawiki
 
 ADD supervisord.conf /etc/supervisor/conf.d/mediawiki.conf
@@ -70,3 +59,16 @@ VOLUME /var/www/mediawiki/extensions
 VOLUME /var/www/mediawiki/images
 
 ENTRYPOINT ["/entrypoint.sh"]
+
+# Build-time metadata as defined at http://label-schema.org
+ARG BUILD_DATE
+ARG VCS_REF
+ARG VERSION
+LABEL org.label-schema.build-date=${BUILD_DATE} \
+      org.label-schema.name="Mediawiki" \
+      org.label-schema.description="Simple Mediawiki Container with mysql-server included." \
+      org.label-schema.url="https://hub.docker.com/r/stucky/mediawiki" \
+      org.label-schema.vcs-ref=${VCS_REF} \
+      org.label-schema.vcs-url="https://github.com/stuckyhm/docker-mediawiki" \
+      org.label-schema.version=${MEDIAWIKI_VERSION}-${BASE_IMAGE_TAG} \
+      org.label-schema.schema-version="1.0"
--- update.sh
+++ update.sh
@@ -4,83 +4,117 @@
 function getLastGithubTag {
 	JSON=$(curl -s https://api.github.com/repos/${1}/${2}/tags | jq '.[0]')
 
-	MEDIAWIKI_TAG_NAME=$(echo ${JSON} | jq '.name' | sed -e 's/"//g')
-	MEDIAWIKI_VERSION=$(echo ${MEDIAWIKI_TAG_NAME} | cut -d "." -f 1,2)
-	MEDIAWIKI_PATCH=$(echo ${MEDIAWIKI_TAG_NAME} | cut -d "." -f 3-)
+	VALUE=$(echo ${JSON} | jq '.name' | sed -e 's/"//g')
 
-	[[ "${MEDIAWIKI_TAG_NAME}" = "" ]] && echo "No mediawiki version tag found." && exit 1
+	[[ "${VALUE}" = "" ]] && echo "No version tag ${1}/${2} for found." && exit 1
+
+	echo ${VALUE}
 }
 
-function getLastDockerImage {
-	BASE_TAG=$(wget -q https://registry.hub.docker.com/v1/repositories/${1}/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F: '{print $3}' | grep ${2} | sort | tail -n 1)
+function getLastDockerImageTag {
+	VALUE=$(wget -q https://registry.hub.docker.com/v1/repositories/${1}/tags -O - | \
+		sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | \
+		tr '}' '\n' | \
+		awk -F: '{print $3}' | \
+		grep ${2} | \
+		sort | \
+		tail -n 1)
 
-	[[ "${BASE_TAG}" = "" ]] && echo "No ubuntu version tag found." && exit 1
+	[[ "${VALUE}" = "" ]] && echo "No version tag for '${1}:${2}*' found." && exit 1
+
+	echo ${VALUE}
 }
 
-function replaceInDockerfile {
+function replaceInDockerfileIfChanged {
 	KEY=${1}
 	VALUE_NEW=${2}
 	VALUE_OLD=$(grep -E "^${KEY}" Dockerfile | sed -e "s/^\(${1}\)//g")
 	LINE=$(grep -n -E "^${KEY}" Dockerfile | cut -d ":" -f 1)
 
-	[[ "${VALUE_NEW}" = "${VALUE_OLD}" ]] && CHANGE=0 && return
-
-	sed -i '' -e "s/^\(${KEY}\).*/\1${VALUE_NEW}/g" Dockerfile
-	echo "Replacing at line ${LINE}: '${KEY}' value '${VALUE_OLD}' with '${VALUE_NEW}'."
-
-	CHANGE=1
+	if [[ "${VALUE_NEW}" = "${VALUE_OLD}" ]]; then
+		echo "No changes line ${LINE}: '${KEY}' value '${VALUE_OLD}'."
+		return 1
+	else
+		sed -i '' -e "s/^\(${KEY}\).*/\1${VALUE_NEW}/g" Dockerfile
+		echo "Replacing at line ${LINE}: '${KEY}' value '${VALUE_OLD}' with '${VALUE_NEW}'."
+		return 0
+	fi
 }
 
-function replateTag {
+function replaceTag {
 	TAG=$(echo ${1} | tr -d '[:space:]')
 	git tag -d "${TAG}"
 	git push origin :refs/tags/"${TAG}"
 	git tag -af "${TAG}" -m "${2}"
 }
 
-while getopts "ct" opt; do
-  case $opt in
-    t)
-      TAGGING=1
-      ;;
-  esac
-done
+function dockerBuildAndPush {
+	export DOCKER_REPO=$1
+	chmod a+x ./hooks/build
+	./hooks/build
 
-getLastGithubTag wikimedia mediawiki
-getLastDockerImage ubuntu bionic-
+	export IMAGE_NAME=${DOCKER_REPO}:latest
+	docker push ${IMAGE_NAME}
 
-replaceInDockerfile "ARG BASE_TAG=" "${BASE_TAG}"
-BASE_CHANGE=${CHANGE}
-replaceInDockerfile "ARG MEDIAWIKI_VERSION=" "${MEDIAWIKI_VERSION}"
-VERSION_CHANGE=${CHANGE}
-replaceInDockerfile "ARG MEDIAWIKI_PATCH=" "${MEDIAWIKI_PATCH}"
-PATCH_CHANGE=${CHANGE}
+	chmod a+x ./hooks/post_push
+	./hooks/post_push
+}
 
-BASE_VERSION=$(echo ${BASE_TAG} | cut -d "-" -f 2)
-
-if [[ ${BASE_CHANGE} -eq 1 ]]; then
-	TEXT="base image to ubuntu:${BASE_TAG}"
-	GIT=1
-fi
-if [[ ${VERSION_CHANGE} -eq 1 ]] || [[ ${PATCH_CHANGE} -eq 1 ]]; then
-	[[ "${TEXT}" != "" ]] && TEXT="${TEXT} and "
-	TEXT="${TEXT}mediawiki to ${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}"
-	GIT=1
-fi
-TEXT="Update ${TEXT}."
-
-echo ${TEXT}
-
-if [[ ${GIT} -eq 1 ]]; then
+function gitCommitAndPush {
 	git add Dockerfile
-	git commit -m "${TEXT}"
+	git commit -m "${COMMIT_TEXT}"
 
 	git push
 
-	if [[ ${TAGGING} -eq 1 ]]; then
-		replateTag "v${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}" "${TEXT}"
-		replateTag "v${MEDIAWIKI_VERSION}.${MEDIAWIKI_PATCH}-${BASE_VERSION}" "${TEXT}"
+	BASE_TAG=$(echo ${DOCKER_IMAGE_TAG} | cut -d "-" -f 2)
+	replaceTag "v${APPLICATION_VERSION_MAJOR}" "${COMMIT_TEXT}"
+	replaceTag "v${APPLICATION_VERSION_MAJOR}-${BASE_TAG}" "${COMMIT_TEXT}"
+	replaceTag "v${APPLICATION_VERSION_MAJOR}.${APPLICATION_VERSION_MINOR}" "${COMMIT_TEXT}"
+	replaceTag "v${APPLICATION_VERSION_MAJOR}.${APPLICATION_VERSION_MINOR}-${BASE_TAG}" "${COMMIT_TEXT}"
+	replaceTag "v${APPLICATION_VERSION_MAJOR}.${APPLICATION_VERSION_MINOR}.${APPLICATION_VERSION_PATCH}" "${COMMIT_TEXT}"
+	replaceTag "v${APPLICATION_VERSION_MAJOR}.${APPLICATION_VERSION_MINOR}.${APPLICATION_VERSION_PATCH}-${BASE_TAG}" "${COMMIT_TEXT}"
 
-		git push --tags
+	git push --tags
+}
+
+function updateDockerfileBaseImage {
+	DOCKER_IMAGE_TAG=$(getLastDockerImageTag ${1} ${2})
+
+	replaceInDockerfileIfChanged "ARG BASE_IMAGE_TAG=" "${DOCKER_IMAGE_TAG}"
+	if [[ ${?} -eq 0 ]]; then
+		COMMIT_TEXT="${COMMIT_TEXT}Update base image to '${1}:${DOCKER_IMAGE_TAG}'."$'\n'
+		BUILD=1
 	fi
+}
+
+function updateApplicationVersion {
+	GITHUB_TAG_NAME=$(getLastGithubTag ${1} ${2})
+
+	APPLICATION_KEY=${3:-APPLICATION}
+
+	replaceInDockerfileIfChanged "ARG ${APPLICATION_KEY}_VERSION=" "${GITHUB_TAG_NAME}"
+	if [[ ${?} -eq 0 ]]; then
+		COMMIT_TEXT="${COMMIT_TEXT}Update ${2} to '${GITHUB_TAG_NAME}'."$'\n'
+		BUILD=1
+	fi
+
+	APPLICATION_VERSION_MAJOR=$(echo ${GITHUB_TAG_NAME} | cut -d "." -f 1)
+	replaceInDockerfileIfChanged "ARG ${APPLICATION_KEY}_VERSION_MAJOR=" ${APPLICATION_VERSION_MAJOR}
+
+	APPLICATION_VERSION_MINOR=$(echo ${GITHUB_TAG_NAME} | cut -d "." -f 2)
+	replaceInDockerfileIfChanged "ARG ${APPLICATION_KEY}_VERSION_MINOR=" ${APPLICATION_VERSION_MINOR}
+
+	APPLICATION_VERSION_PATCH=$(echo ${GITHUB_TAG_NAME} | cut -d "." -f 3)
+	replaceInDockerfileIfChanged "ARG ${APPLICATION_KEY}_VERSION_PATCH=" ${APPLICATION_VERSION_PATCH}
+}
+
+
+updateDockerfileBaseImage ubuntu bionic-
+updateApplicationVersion wikimedia mediawiki MEDIAWIKI
+
+echo ${COMMIT_TEXT}
+
+if [[ ${BUILD} -eq 1 ]]; then
+	gitCommitAndPush
+	dockerBuildAndPush stucky/mediawiki
 fi
